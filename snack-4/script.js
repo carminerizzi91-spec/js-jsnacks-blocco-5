da9a9244fc6052cdfab1763ac1413776049f08af
carminerizzi91-spec~ <carminerizzi91@gmail.com>
2025-11-13 20:14:02 +0100

    add: esercizio 3 e 4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,9 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+const names = [];
+posts.forEach(post => {
+  names.push(post.author)
+});
+
+console.log(names);
