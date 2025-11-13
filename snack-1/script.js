c262b92755472b4a12b9343af476a1284a4e0b85
carminerizzi91-spec~ <carminerizzi91@gmail.com>
2025-11-13 20:02:16 +0100

    first commit

---
+++ snack-1/script.js
@@ -0,0 +1,8 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+names.forEach(function(name, index){
+    console.log(name);
+});
